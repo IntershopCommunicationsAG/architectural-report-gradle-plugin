7e92ef0e8305d6373f5e83a04c68e5d019e9907a
Thomas Bergmann <t.bergmann@intershop.de>
2019-01-12 14:59:41 +0100

    re-add filter existing issues

--- src/main/java/com/intershop/tool/architecture/report/common/issue/IssuePrinter.java
+++ src/main/java/com/intershop/tool/architecture/report/common/issue/IssuePrinter.java
@@ -2,10 +2,15 @@ package com.intershop.tool.architecture.report.common.issue;
 
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.Comparator;
 import java.util.Formatter;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
@@ -15,6 +20,8 @@ import org.slf4j.LoggerFactory;
 import com.intershop.tool.architecture.report.cmd.ArchitectureReportConstants;
 import com.intershop.tool.architecture.report.cmd.ArchitectureReportOutputFolder;
 import com.intershop.tool.architecture.report.cmd.CommandLineArguments;
+import com.intershop.tool.architecture.report.common.resources.URILoader;
+import com.intershop.tool.architecture.report.common.resources.XMLLoaderException;
 
 public class IssuePrinter
 {
@@ -58,7 +65,8 @@ public class IssuePrinter
             List<String> keySelector = Arrays.asList(keys.split(","));
             stream = stream.filter(i -> keySelector.contains(i.getKey()));
         }
-        return stream.sorted(ISSUE_COMPARATOR).collect(Collectors.toList());
+        Map<String, JiraIssue> existingIssues = getExistingIssues();
+        return stream.filter(i -> !existingIssues.containsKey(i.getHash())).sorted(ISSUE_COMPARATOR).collect(Collectors.toList());
     }
 
     private boolean printFilteredIssues(List<Issue> filteredIssues)
@@ -90,5 +98,29 @@ public class IssuePrinter
         }
         LOGGER.error("Architecture report contains new errors, see '{}'.", newIssuesFile.getAbsolutePath());
         return false;
+    }
+
+    private Map<String, JiraIssue> getExistingIssues()
+    {
+        String jiraIssueLocation = info.getArgument(ArchitectureReportConstants.ARG_EXISTING_ISSUES_FILE);
+        if (jiraIssueLocation == null)
+        {
+            return Collections.emptyMap();
+        }
+        Map<String, JiraIssue> result = new HashMap<>();
+        try
+        {
+            InputStream is = URILoader.getInputStream(jiraIssueLocation);
+            if (is != null)
+            {
+                List<JiraIssue> issues = new JiraIssuesVisitor().apply(is);
+                issues.stream().forEach(issue -> result.put(issue.getKey(), issue));
+            }
+        }
+        catch(XMLLoaderException|IOException e)
+        {
+            LoggerFactory.getLogger(IssuePrinter.class).warn("Can't load issues file: " + jiraIssueLocation, e);
+        }
+        return result;
     }
 }
