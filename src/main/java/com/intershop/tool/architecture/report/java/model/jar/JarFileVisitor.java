accf118280ec33324d7b2e4dde4cc88f2c7b6097
DevDavido <997605+DevDavido@users.noreply.github.com>
2022-05-16 17:56:29 +0200

    fix: Add logging to JAR file visitor to see progress (#76079, #76366)

--- src/main/java/com/intershop/tool/architecture/report/java/model/jar/JarFileVisitor.java
+++ src/main/java/com/intershop/tool/architecture/report/java/model/jar/JarFileVisitor.java
@@ -5,6 +5,19 @@
  */
 package com.intershop.tool.architecture.report.java.model.jar;
 
+import com.intershop.tool.architecture.report.common.project.ProjectRef;
+import com.intershop.tool.architecture.report.common.resources.XMLInputSourceVisitor;
+import com.intershop.tool.architecture.report.java.model.jclass.JavaClass;
+import com.intershop.tool.architecture.report.java.model.jclass.JavaClassVisitor;
+import com.intershop.tool.architecture.report.java.model.jclass.JavaHelper;
+import com.intershop.tool.architecture.report.java.model.pipelet.PipeletDescriptor;
+import com.intershop.tool.architecture.report.java.model.pipelet.PipeletHandler;
+import org.objectweb.asm.ClassReader;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.xml.sax.SAXException;
+
+import javax.xml.parsers.ParserConfigurationException;
 import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
@@ -14,25 +27,13 @@ import java.util.function.Function;
 import java.util.jar.JarEntry;
 import java.util.jar.JarFile;
 
-import javax.xml.parsers.ParserConfigurationException;
-
-import org.objectweb.asm.ClassReader;
-import org.xml.sax.SAXException;
-
-import com.intershop.tool.architecture.report.common.project.ProjectRef;
-import com.intershop.tool.architecture.report.common.resources.XMLInputSourceVisitor;
-import com.intershop.tool.architecture.report.java.model.jclass.JavaClass;
-import com.intershop.tool.architecture.report.java.model.jclass.JavaClassVisitor;
-import com.intershop.tool.architecture.report.java.model.jclass.JavaHelper;
-import com.intershop.tool.architecture.report.java.model.pipelet.PipeletDescriptor;
-import com.intershop.tool.architecture.report.java.model.pipelet.PipeletHandler;
-
 /**
  * A file visitor that analysis Jar files.
  */
 public class JarFileVisitor implements Function<ClassReader, JavaClass>
 {
-    private XMLInputSourceVisitor xmlVisitor = new XMLInputSourceVisitor();
+    private static final Logger logger = LoggerFactory.getLogger(JarFileVisitor.class);
+    private final XMLInputSourceVisitor xmlVisitor = new XMLInputSourceVisitor();
     private final ProjectRef projectRef;
 
     public JarFileVisitor(ProjectRef projectRef)
@@ -65,6 +66,7 @@ public class JarFileVisitor implements Function<ClassReader, JavaClass>
         Jar result = new Jar(projectRef);
         try (JarFile jarFile = new JarFile(file))
         {
+            logger.info("Reading files in JAR: {}", file);
             Enumeration<JarEntry> entries = jarFile.entries();
             while(entries.hasMoreElements())
             {
